9027c05de921fa20b67af5d2f33ecc112fcb8ff3
rtarun9 <rtarun0913@gmail.com>
2023-05-17 13:16:23 +0530

    Updated database config file to connect to remote database

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'Pravin15@',
-  database: 'Smart_Attendance'
+  host: 'sql12.freemysqlhosting.net',
+  user: 'sql12617971',
+  password: '67FC8yGtph',
+  database: 'sql12617971',
+  port: 3306
 });
 
 connection.connect((err) => {
