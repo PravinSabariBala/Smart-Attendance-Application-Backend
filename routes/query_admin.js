677fabf3534dd350a7b09ea538f2f8d14b7d71ef
PravinSabariBala <pravinpsb16@gmail.com>
2023-06-12 22:23:16 +0530

    fixed admin

--- routes/query_admin.js
+++ routes/query_admin.js
@@ -7,16 +7,29 @@ router.post("/", (req, res) => {
     res.send("Error: No query found");
     return;
   }
-  
-  
-
   connection.query(req.body.query, function(err, result, fields) {
     if (err) {
       res.send(err);
       return;
     }
-    res.send(result);
+    if (req.body.query1){
+      connection.query(req.body.query1, function(err, result1, fields) {
+        if (err) {
+          res.send(err);
+          return;
+        }
+        console.log(result)
+        console.log(result1)
+        
+        res.send(result+result1);
+      });
+    }else{
+      res.send(result)
+    }
+    
   });
+  
+  
 });
 
 module.exports = router;
